17cf26aff75806db24f466efae51a770bf6acfd4
myrslint <qemu.haziness801@passinbox.com>
2025-08-15 16:53:09 +0000

    KVM: i386: Default disable ignore guest PAT quirk
    Add a new accelerator option that allows the guest to adjust the PAT.
    This is already the case for TDX guests and allows using virtio-gpu
    Venus with RADV or NVIDIA drivers.
    
    The quirk is disabled by default.  Since this caused problems with
    Linux's Bochs video device driver, add a knob to leave it enabled,
    and for now do ont enable it by default.
    
    Signed-off-by: Myrsky Lintu <qemu.haziness801@passinbox.com>
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/2943
    Link: https://lore.kernel.org/r/175527721636.15451.4393515241478547957-1@git.sr.ht
    [Add property; for now leave it off by default. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -3946,6 +3946,7 @@ static void kvm_accel_instance_init(Object *obj)
     s->xen_evtchn_max_pirq = 256;
     s->device = NULL;
     s->msr_energy.enable = false;
+    s->honor_guest_pat = ON_OFF_AUTO_OFF;
 }
 
 /**
